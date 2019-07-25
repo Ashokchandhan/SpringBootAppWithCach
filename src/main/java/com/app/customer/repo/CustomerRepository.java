e2a555c8c41e99c83932631c7e14917c6cbb9ccd
new <new@DESKTOP-TCT062V>
2019-07-25 10:33:57 +0530

    comite

---
+++ src/main/java/com/app/customer/repo/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.app.customer.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.model.Customer;
+
+public interface CustomerRepository extends
+JpaRepository<Customer, Integer> {
+
+}
